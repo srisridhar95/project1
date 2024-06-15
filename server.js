15534168f99ad89a9b5633c50ea152e26147b52a
srisridhar950 <vaddepellisridhar9494@gmil.com>
2024-06-15 19:49:37 +0530

    chane

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello DevOps Engineer, This Is the First Project.............');
+res.send('Hello DevOps Engineer, This Is the First Project.............Sridhar Vaddepelli');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
